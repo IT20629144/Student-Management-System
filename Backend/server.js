652f2fc845341d1df53b1d10a7ec749929a4f88b
IT20625634 <85445037+RavinduNirmal@users.noreply.github.com>
2022-05-29 00:42:30 +0530

    Database Connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express'); 
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = 8000;
 
+const DB_URL = 'mongodb+srv://ravindunirmal:ravindunirmal123@cluster0.sibwe6h.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL)
+ .then(() =>{
+     console.log('DB Connected');
+ })
+  .catch((err) => console.log('DB Connecting Error' ,err));
+  
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
-})
+});
+
