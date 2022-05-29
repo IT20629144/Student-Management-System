e910aa31521d45ccfe1234c30bcf1284c95367c3
IT20625634 <85445037+RavinduNirmal@users.noreply.github.com>
2022-05-29 14:54:56 +0530

    GetAllStudentFunction created

--- Backend/Routes/student.js
+++ Backend/Routes/student.js
@@ -1,4 +1,6 @@
 const express = require ('express');
+const { find } = require('../models/student');
+
 
 const Students = require('../models/student');
 
@@ -9,7 +11,7 @@ const router = express.Router();
 
 
 router.post('/student/add',(req,res) => {
-
+ 
      let newStudent = new Students(req.body);
 
      newStudent.save((err) =>{
@@ -24,6 +26,41 @@ router.post('/student/add',(req,res) => {
             success:"Student Saved Successfully"
         });
      });
+});
+
+//get all students
+
+// router.get('/student/studentall',(req,res) =>{
+
+//     Students.find().exec((err,students) =>{
+//         if(err){
+//             return res.status(400).json({
+//                 error:err
+//           });
+//         }
+
+//         return res.status(200).json({
+//             success:true,
+//             existingPosts:students
+//         });
+//     });
+// });
+
+router.get('/student/studentall',(req,res) =>{
+    
+    Students.find().exec((err,students) =>{
+        if(err){
+            return res.status(400).json({
+               
+                error:err
+            });
+        }
+
+        return res.status(200).json({
+            success:true,
+            exeistingStudents:students
+        });
+    });
 });
 
 module.exports = router;
